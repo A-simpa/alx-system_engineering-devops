f54c919cbb45fb8ad1e7b91987112500c8e88605
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-09-18 16:43:14 +0000

    tpuppet stdlib

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 #configures an ssh config file to contain some content
 include stdlib
 
-file_line {'/etc/ssh/ssh_config':
+file_line {'Turn off passwd auth':
     ensure  => present,
     path    => '/etc/ssh/ssh_config',
     line    => '	PasswordAuthentication no',
     replace => true
 }
 
-file_line {'/etc/ssh/ssh_config':
+file_line {'Declare identity file':
     ensure  => present,
     path    => '/etc/ssh/ssh_config',
-    line    => '		IdentityFile ~/.ssh/school',
+    line    => '	IdentityFile ~/.ssh/school',
     replace => true
 }
