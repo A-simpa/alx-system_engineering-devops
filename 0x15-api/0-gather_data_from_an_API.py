9aeeab2ba215f8fbab733bb4c8f1d1351e78d670
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-17 21:35:17 +0000

    here is the api task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,22 +6,22 @@ import sys
 
 
 def main():
-    complete_count, userId = 0, int(sys.argv[1])
-    start = (userId - 1) * 20
-    end = userId * 20
-    name = requests.get(f'https://jsonplaceholder.typicode.com/users/{userId}')
+    done, userId = 0, sys.argv[1]
+    start = (int(userId) - 1) * 20
+    end = int(userId) * 20
+    name = requests.get('https://jsonplaceholder.typicode.com/users/' + userId)
     name = name.json()['name']
     resp = requests.get(f'https://jsonplaceholder.typicode.com/todos').json()
     resp = resp[start:end]
 
     for task in resp:
         if (task['completed']):
-            complete_count += 1
+            done += 1
 
-    print(f'Employee {name} is done with tasks({complete_count}/20)')
+    print('Employee {} is done with tasks({}/20)'.format(name, done))
     for task in resp:
         if (task['completed']):
-            print(f'\t {task["title"]}')
+            print('\t {}'.format(task['title']))
 
 
 if __name__ == '__main__':
