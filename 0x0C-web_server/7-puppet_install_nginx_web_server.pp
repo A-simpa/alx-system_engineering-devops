63ece3292b4bafb69ad72310fff753f29a957827
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-12 03:41:49 +0000

    done here

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,31 +6,31 @@
 include stdlib
 
 class {'configure nginx':
-	exec {'update apt-get':
-		command => '/usr/bin/apt-get update',
-	}
+  exec {'update apt-get':
+    command => '/usr/bin/apt-get update',
+  }
 
-	exec {'install nginx':
-		command => '/usr/bin/apt-get -y install nginx',
-	}
+  exec {'install nginx':
+    command => '/usr/bin/apt-get -y install nginx',
+  }
 
-	exec {'firewall allow http':
-		command => '/usr/sbin/ufw allow "Nginx HTTP"',
-	}
+  exec {'firewall allow http':
+    command => '/usr/sbin/ufw allow "Nginx HTTP"',
+  }
 
-	file {'/var/www/html/index.html':
-		content => 'Hello World!',
-		ensure => present,
-	}
+  file {'/var/www/html/index.html':
+    content => 'Hello World!',
+    ensure  => present,
+  }
 
-	file_line {'redirection':
-		path => '/etc/nginx/sites-enabled/default',
-		after => '^Server_name _;',
-		line => '		location = /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }',
-	}
+  file_line {'redirection':
+    path  => '/etc/nginx/sites-enabled/default',
+    after => '^Server_name _;',
+    line  => '		location = /redirect_me { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }',
+  }
 
-	service {'nginx':
-		name => 'nginx',
-		restart => ,
-	}
+  service {'nginx':
+    name    => 'nginx',
+    restart => ,
+  }
 }
