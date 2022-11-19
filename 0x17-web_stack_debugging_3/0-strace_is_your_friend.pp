9ed6710af9a88f67f4acbe070235f57cf8c86217
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-19 18:40:42 +0000

    fixed configuration

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#change typo in php config file
+
+exec {'fix php typo':
+  command        => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+        provider =>
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+Debugging an apache2 returning an internal error
